eef73a476d990e386a38a008729a222b17e9bf5b
JangoBrick <mail@jangobrick.de>
2016-08-06 18:13:58 +0200

    Implement preview functionality
    Adds a button to the first box that, when clicked, opens a file selector
    for images. When an image is selected, it is read via the FileReader API
    and then displayed on all '.previmg' elements.

--- app/app.js
+++ app/app.js
@@ -2,8 +2,57 @@
 
 var app = angular.module("youtube-thumbnail-preview", []);
 
+
+
 app.controller("main", ["$scope", function ($scope) {
 
-    
+    $scope.imageFiles = null;
+    $scope.image = null;
 
+    $scope.$watch("imageFiles", function (files) {
+
+        if (!files || !files.length)
+            return;
+
+        var file = files[0];
+
+        var reader = new FileReader();
+        reader.onload = function (event) {
+            $scope.$apply(function () {
+                $scope.image = event.target.result;
+            });
+        };
+        reader.readAsDataURL(file);
+
+        $scope.imageFiles = null;
+
+    });
+
+}]);
+
+
+
+app.directive("fileModel", [function () {
+    return {
+
+        restrict: "A",
+        require: "ngModel",
+
+        link: function (scope, element, attrs, ctrl) {
+
+            // file select -> model
+            element.bind("change", function (event) {
+                ctrl.$setViewValue(event.target.files);
+            });
+
+            // model -> file select
+            ctrl.$render = function () {
+                if (!ctrl.$viewValue) {
+                    element.val(null);
+                }
+            };
+
+        }
+
+    };
 }]);
