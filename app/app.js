cf4c92d2f2615a10af93b6f1ab91077b6157bd3c
JangoBrick <mail@jangobrick.de>
2016-12-23 11:43:47 +0100

    Restructure app.js code

--- app/app.js
+++ app/app.js
@@ -1,29 +1,27 @@
 /*global angular */
 
-var app = angular.module("youtube-thumbnail-preview", ["ngFilemodel"]);
+angular.module("youtube-thumbnail-preview", ["ngFilemodel"])
 
+.controller("main", ["$scope", function ($scope) {
 
-
-app.controller("main", ["$scope", function ($scope) {
-
-    $scope.imageFiles = null;
-    $scope.image = null;
-
+    /** Stores info about the fake video that is displayed. */
     $scope.info = {
         title: "Title of the video",
         uploader: "Channel Name",
         time: "12:34",
         uploadtime: "42 minutes ago",
-        views: "123,456"
+        views: "123,456",
     };
 
-    $scope.$watchCollection("imageFiles", function (files) {
+    /** FileList model for the thumbnail selection */
+    $scope.imageFiles = null;
+    /** The currently displayed data URL */
+    $scope.image = null;
 
-        if (!files || !files.length)
-            return;
-
-        var file = files[0];
-
+    /**
+     * Reads the given File object as a data URL and stores it in $scope.image.
+     */
+    function loadFile(file) {
         var reader = new FileReader();
         reader.onload = function (event) {
             $scope.$apply(function () {
@@ -31,20 +29,21 @@ app.controller("main", ["$scope", function ($scope) {
             });
         };
         reader.readAsDataURL(file);
+    }
 
+    // load thumbnail on file selection
+    $scope.$watchCollection("imageFiles", function (files) {
+        if (files && files.length) {
+            loadFile(files[0]);
+        }
     });
 
-}]);
+}])
 
-
-
-app.directive("appThumbnail", [function () {
+.directive("appThumbnail", [function () {
     return {
-
         restrict: "E",
         scope: true,
-
-        templateUrl: "app/thumbnail.html"
-
+        templateUrl: "app/thumbnail.html",
     };
 }]);
